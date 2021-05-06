68e12584a769998eb1cf6ca25537cd12c8884e4a
rossum <rossum@tuta.io>
2021-05-06 19:19:50 +0200

    rates api fix

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1,6 +1,6 @@
 export const fetchRate = async () =>  {
     try {
-        const response = await fetch('https://api.exchangeratesapi.io/latest?symbols=HRK');
+        const response = await fetch('https://api.ratesapi.io/api/latest?symbols=HRK');
         const json = await response.json();
         return json.rates.HRK;
     }
